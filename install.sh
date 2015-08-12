f80183623c917e7fb8f76469b5ca08397e4e9261
Sen Zhang <szhang.hust@gmail.com>
2015-08-12 23:03:29 +0800

    fix the bug in install.sh

--- install.sh
+++ install.sh
@@ -6,40 +6,17 @@
 # set vim dir
 vim_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# check if git is installed
-hash git >/dev/null && /usr/bin/env git clone git://github.com/kaochenlong/eddie-vim2.git ~/.vim || {
-  echo "Sorry, Git is not installed in this machine."
-  echo "Should install Git first"
-  exit
-}
-
-# check files exist and backup
-vim_folder=~/.vim
 vim_rc=~/.vimrc
 gvim_rc=~/.gvimrc
 backup_rand=$RANDOM
 
-## detect if there's a .vim folder
-if [ -d $vim_folder ]
-then
-  echo "\033[0;31mYou already have a .vim folder in your home directory.\033[0;m"
-  read -p "Would you like to backup your .vim folder first? [y/n] " ans
-  if [ "$ans" == "y" ]
-  then
-    echo "backup your original $vim_folder to $vim_folder-$(date +%Y%m%d)-$backup_rand-bak"
-    mv $vim_folder $vim_folder-$(date +%Y%m%d)-$backup_rand-bak
-  else
-    exit
-  fi
-fi
 
 
-echo $vim_rc
-
+# check files exist and backup
 ## check if vimrc is already exist
 if [ -L $vim_rc ] || [ -f $vim_rc ]
 then
-  echo "\033[0;31mThere's a .vimrc in your home directory.\033[0;m"
+  echo "There's a .vimrc in your home directory."
   read -p "Would you like to backup your .vimrc first? [y/n] " ans
   if [ "$ans" == "y" ]
   then
@@ -51,7 +28,7 @@ fi
 ## check if gvimrc is already exist
 if [ -L $gvim_rc ] || [ -f $gvim_rc ]
 then
-  echo "\033[0;31mThere's a .gvimrc in your home directory.\033[0;m"
+  echo "There's a .gvimrc in your home directory."
   read -p "Would you like to backup your .gvimrc first? [y/n] " ans
   if [ "$ans" == "y" ]
   then
@@ -60,24 +37,42 @@ then
   fi
 fi
 
-# install vundle
+
+
+# install vundle and plugin
+## install vundle
 if [ -d ./bundle/vundle ]; then
   rm -rf ./bundle/vundle
 fi
 
 git clone https://github.com/gmarik/Vundle.vim ./bundle/vundle
 
-# install plugin
+## install plugin
 vim +PluginInstall +qall
 
-# linking to .vimrc
-ln -s $vim_dir/vimrc ~/.vimrc
 
-# linking to .gvimrc
-ln -s $vim_dir/gvimrc ~/.gvimrc
+
+# linking to .rc
+## linking to .vimrc
+ln -s $vim_dir/vimrc $vim_rc
+
+## linking to .gvimrc
+ln -s $vim_dir/gvimrc $gvim_rc
+
+
 
 # install fonts for airline
 ./bundle/powerline_fonts/install.sh
 
-echo "\033[0;34mNice! Seems everything is done.\033[0m"
 
+
+# installed successfully
+echo ""
+echo "Installed the Vim configuration successfully!"
+echo ""
+echo "Structure:"
+echo ".vimrc        :  $vim_rc"
+echo ".gvimrc       :  $gvim_rc"
+echo "Config Folder :  $vim_dir"
+echo ""
+echo "Enjoy it :-)"
