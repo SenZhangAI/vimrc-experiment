c27e6f21635cc420c1c9e8275572d4060d612882
Sen Zhang <szhang.hust@gmail.com>
2015-08-12 20:56:30 +0800

    reorganization code

--- install.sh
+++ install.sh
@@ -1,7 +1,64 @@
 #!/bin/bash
 
+# Some of this scripts come from:
+# Github Repository: https://github.com/kaochenlong/eddie-vim2\
+
 # set vim dir
 vim_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# check if git is installed
+hash git >/dev/null && /usr/bin/env git clone git://github.com/kaochenlong/eddie-vim2.git ~/.vim || {
+  echo "Sorry, Git is not installed in this machine."
+  echo "Should install Git first"
+  exit
+}
+
+# check files exist and backup
+vim_folder=~/.vim
+vim_rc=~/.vimrc
+gvim_rc=~/.gvimrc
+backup_rand=$RANDOM
+
+## detect if there's a .vim folder
+if [ -d $vim_folder ]
+then
+  echo "\033[0;31mYou already have a .vim folder in your home directory.\033[0;m"
+  read -p "Would you like to backup your .vim folder first? [y/n] " ans
+  if [ "$ans" == "y" ]
+  then
+    echo "backup your original $vim_folder to $vim_folder-$(date +%Y%m%d)-$backup_rand-bak"
+    mv $vim_folder $vim_folder-$(date +%Y%m%d)-$backup_rand-bak
+  else
+    exit
+  fi
+fi
+
+
+echo $vim_rc
+
+## check if vimrc is already exist
+if [ -L $vim_rc ] || [ -f $vim_rc ]
+then
+  echo "\033[0;31mThere's a .vimrc in your home directory.\033[0;m"
+  read -p "Would you like to backup your .vimrc first? [y/n] " ans
+  if [ "$ans" == "y" ]
+  then
+    echo "backup your original $vim_rc to $vim_rc-$(date +%Y%m%d)-$backup_rand-bak"
+    mv $vim_rc $vim_rc$(date +%Y%m%d)-$backup_rand-bak
+  fi
+fi
+
+## check if gvimrc is already exist
+if [ -L $gvim_rc ] || [ -f $gvim_rc ]
+then
+  echo "\033[0;31mThere's a .gvimrc in your home directory.\033[0;m"
+  read -p "Would you like to backup your .gvimrc first? [y/n] " ans
+  if [ "$ans" == "y" ]
+  then
+    echo "backup your original $gvim_rc to $gvim_rc-$(date +%Y%m%d)-$backup_rand-bak"
+    mv $gvim_rc $gvim_rc$(date +%Y%m%d)-$backup_rand-bak
+  fi
+fi
 
 # install vundle
 if [ -d ./bundle/vundle ]; then
@@ -14,22 +71,13 @@ git clone https://github.com/gmarik/Vundle.vim ./bundle/vundle
 vim +PluginInstall +qall
 
 # linking to .vimrc
-if [ -f ~/.vimrc ]; then
-  echo ".vimrc is existing, move to .vimrc_bak"
-  mv ~/.vimrc ~/.vimrc_bak
-fi
-
-ln -s $vim_dir/vimrc_easy ~/.vimrc
+ln -s $vim_dir/vimrc ~/.vimrc
 
 # linking to .gvimrc
-if [ -f ~/.gvimrc ]; then
-  echo ".gvimrc is existing, move to .gvimrc_bak"
-  mv ~/.gvimrc ~/.gvimrc_bak
-fi
-
 ln -s $vim_dir/gvimrc ~/.gvimrc
 
 # install fonts for airline
 ./bundle/powerline_fonts/install.sh
 
+echo "\033[0;34mNice! Seems everything is done.\033[0m"
 
--- vimrc
+++ vimrc
@@ -1,4 +1,4 @@
-" Pro version Vimrc
+" Easy version Vimrc
 " I kept all other settings in plugins/settings directory
 "
 " Eddie Kao
@@ -6,4 +6,26 @@
 " eddie@digik.com.tw
 
 execute pathogen#infect()
+
+" custom key
+" add a new line without entering insert mode
+noremap <CR> o<Esc>
+
+" map backspce to delete a character
+noremap <BS> X
+
+" map ctrl+c and ctrl+v
+"noremap <C-c> y
+"noremap <C-v> P
+
+" navigating for long lines
+map j gj
+map k gk
+map <UP> gk
+map <DOWN> gj
+"imap <UP> <ESC>gk<RIGHT>i    "these have a bug that can't work well.
+"imap <DOWN> <ESC>gj<RIGHT>i
+
+" select ALL
+map <C-A> ggVG
 
